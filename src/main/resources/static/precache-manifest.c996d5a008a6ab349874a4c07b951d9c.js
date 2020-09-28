244fdb2a6f98fb45295516337552b7cb2b85dc06
nagasrinivask <naga.kapusetti.consultant@nielsen.com>
2020-09-28 17:16:19 +0530

    Initial Commit

---
+++ src/main/resources/static/precache-manifest.c996d5a008a6ab349874a4c07b951d9c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd033c1306cef5ea6a6d61ea670ad029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964fd4cd75409fc70a5b",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "1e1e3f3faa1586b5078a",
+    "url": "/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "964fd4cd75409fc70a5b",
+    "url": "/static/js/2.07d3a620.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/static/js/2.07d3a620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1e3f3faa1586b5078a",
+    "url": "/static/js/main.bb3db7b1.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/static/js/main.bb3db7b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1948da6512e239c4cd",
+    "url": "/static/js/runtime-main.f9fa5b6e.js"
+  },
+  {
+    "revision": "db224b88ff70cbe220411ca7d2de2ebe",
+    "url": "/static/media/sidebar-5.db224b88.jpg"
+  }
+]);
---
+++ src/test/java/com/ng/financialliteracy/FinancialLiteracyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ng.financialliteracy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialLiteracyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
