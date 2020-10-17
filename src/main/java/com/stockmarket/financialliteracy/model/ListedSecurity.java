9aa4146a4bc280d4dc7838f8cf8e09c7bb35e74a
maheshkarri <mahesh.karri.consultant@nieslen.com>
2020-10-17 15:00:33 +0530

    Added listed securities

---
+++ src/main/java/com/stockmarket/financialliteracy/model/ListedSecurity.java
@@ -0,0 +1,34 @@
+package com.stockmarket.financialliteracy.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "LISTED_SECURITY", schema = "public")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ListedSecurity {
+
+    @Id
+    @JsonProperty("SYMBOL")
+    @Column(name = "SYMBOL")
+    private String symbol;
+
+    @JsonProperty("COMPANY_NAME")
+    @Column(name = "COMPANY_NAME")
+    private String companyName;
+
+    @JsonProperty("SERIES")
+    @Column(name = "SERIES")
+    private String series;
+
+    @JsonProperty("EXCHANGE")
+    @Column(name = "EXCHANGE")
+    private String exchange;
+}
